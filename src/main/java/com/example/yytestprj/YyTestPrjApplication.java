61095191593db759cdd0de736220e80d682ee7b5
yongyong92 <dorong@tivansoft.com>
2025-01-24 10:30:13 +0900

    최초 등록

---
+++ src/main/java/com/example/yytestprj/YyTestPrjApplication.java
@@ -0,0 +1,13 @@
+package com.example.yytestprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyTestPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyTestPrjApplication.class, args);
+    }
+
+}
